8c7d58925df89b8f9d95eaf1664f0ec407fd2bf1
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-16 08:56:43 +0200

    chore: update the other branch

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 import { api } from "@mp/api-client/react";
 
 export function App() {
-  const query = api.example.greeting.useQuery("Testing client");
+  const query = api.example.greeting.useQuery("Another client");
   return (
     <>
       <h1>Hi Mom!</h1>
