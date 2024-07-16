4f0c713df859a40ae6ffdf7b2042331d23a485c4
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-16 09:51:54 +0200

    ci: add import linting plugins

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import unusedImportsPlugin from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
   {
@@ -13,6 +15,23 @@ export default tseslint.config(
   {
     rules: {
       "no-undef": "off", // Gives false negatives. We already have typescript to catch these.
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+      "unused-imports": unusedImportsPlugin,
+    },
+    rules: {
+      // Consistent order of imports makes a modules dependencies easier to grasp mentally for humans
+      "import/order": ["error"],
+
+      // Automatically removes unused imports. Reduces need for tree shaking in builds.
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/no-unused-vars": "off", // Turning off no-unused-vars is recommended by the unused-imports plugin
+
+      // Automatically enforces type-only imports. Reduces need for tree shaking in builds.
+      "@typescript-eslint/consistent-type-imports": "warn",
     },
   },
 );
