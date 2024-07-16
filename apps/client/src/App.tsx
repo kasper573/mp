e7b005acb2175b05a620790531397fcf0a34955f
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-16 11:15:45 +0200

    feat: add count example

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,10 +2,15 @@ import { api } from "@mp/api-client/react";
 
 export function App() {
   const query = api.example.greeting.useQuery("Main client");
+  const count = api.example.count.useQuery();
+  const increment = api.example.increaseCount.useMutation();
+
   return (
     <>
       <h1>Hi Mom!</h1>
       <p>{query.data}</p>
+      <p>Count: {count.data}</p>
+      <button onClick={() => increment.mutate({ amount: 1 })}>Increment</button>
     </>
   );
 }
