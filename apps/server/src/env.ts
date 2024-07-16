cafcbb63e82ea72f3f6c9403e175b6ff6ce245f7
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-16 12:25:51 +0200

    fix: remove unused env var

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -2,12 +2,10 @@ import { z, mode, numeric } from "@mp/validate";
 
 const schema = z.object({
   mode: mode.default("development"),
-  logFormat: z.enum(["tiny", "short", "dev", "combined"]).default("combined"),
   port: numeric.default(1234),
 });
 
 export const env = schema.parse({
   mode: process.env.NODE_ENV,
-  logFormat: process.env.LOG_FORMAT,
   port: process.env.PORT,
 });
