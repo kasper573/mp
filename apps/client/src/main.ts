839630ca44dcb8e46c0621e2913033a5ea8b94cf
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-18 23:34:21 +0200

    feat: add cheat button

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -3,6 +3,7 @@ import { api } from "./api";
 const chat = document.querySelector("textarea")!;
 const input = document.querySelector("input")!;
 const form = document.querySelector("form")!;
+const cheatButton = document.querySelector("button#try-to-cheat")!;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,4 +13,8 @@ form.addEventListener("submit", (e) => {
 
 api.subscribe("example", "chat", (message) => {
   chat.value += `${message.from}: ${message.contents}\n`;
+});
+
+cheatButton.addEventListener("click", () => {
+  api.send("example", "chat", { from: "server", contents: "I'm cheating!" });
 });
