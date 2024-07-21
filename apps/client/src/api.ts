981eb1997ba4391b1c73b8a19f9f8616faa03134
Kasper <805006+kasper573@users.noreply.github.com>
2024-07-21 17:47:01 +0200

    fix: remove unused types

--- apps/client/src/api.ts
+++ apps/client/src/api.ts
@@ -2,8 +2,6 @@ import { Client, Logger } from "@mp/tsock/client";
 import type { ClientContext, ClientState, ServerModules } from "@mp/server";
 import { env } from "./env";
 
-export type * as types from "@mp/server";
-
 export const { modules: events, subscribeToState } = new Client<
   ServerModules,
   ClientContext,
