f1b1aeb577bed4080ff21dd0f2defa38fee25418
Kasper <805006+kasper573@users.noreply.github.com>
2024-08-03 22:06:14 +0200

    fix: new server env var names

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -1,7 +1,7 @@
 export const env = {
-  host: process.env.MP_SERVER_HOST || "host-missing",
-  wsPort: parseInt(process.env.MP_SERVER_WS_PORT!) || -1,
-  httpPort: parseInt(process.env.MP_SERVER_HTTP_PORT!) || -1,
-  httpCorsOrigin: process.env.MP_SERVER_HTTP_CORS_ORIGIN || "origin-missing",
-  tickInterval: parseInt(process.env.MP_SERVER_TICK_INTERVAL!) || 1000 / 60,
+  host: process.env.HOST || "host-missing",
+  wsPort: parseInt(process.env.WS_PORT!) || -1,
+  httpPort: parseInt(process.env.PORT!) || -1,
+  httpCorsOrigin: process.env.CORS_ORIGIN || "origin-missing",
+  tickInterval: parseInt(process.env.TICK_INTERVAL!) || 1000 / 60,
 };
