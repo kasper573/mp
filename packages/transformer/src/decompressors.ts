4c83c71084379d124ce35a6bbd4bf71890b6b771
Kasper <805006+kasper573@users.noreply.github.com>
2024-08-19 07:41:14 +0200

    fix: gzip support

--- packages/transformer/src/decompressors.ts
+++ packages/transformer/src/decompressors.ts
@@ -5,9 +5,7 @@ export const decompressors = {
   zstd() {
     throw new Error("Not implemented");
   },
-  gzip() {
-    throw new Error("Not implemented");
-  },
+  gzip: pako.ungzip,
 } satisfies Record<string, Decompressor>;
 
 export type Decompressor = (str: Uint8Array) => Uint8Array;
