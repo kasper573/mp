6c20d082546763a511489ce34134069323f6d6e9
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-01 17:25:23 +0200

    fix: non null assertion is not a javascript thing

--- apps/server/drizzle.config.js
+++ apps/server/drizzle.config.js
@@ -1,4 +1,4 @@
-// Important: Do not import anything in this file, 
+// Important: Do not import anything in this file,
 // since it's uploaded isolated to the server as part of deployment
 
 /** @type { import("drizzle-kit").Config } */
@@ -6,6 +6,6 @@ export default {
   schema: "./src/modules/*/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.MP_DATABASE_URL!,
+    url: process.env.MP_DATABASE_URL,
   },
 };
