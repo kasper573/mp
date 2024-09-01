09fbbcec44656174ac75e54400b9ebba35ad02af
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-01 17:24:09 +0200

    fix: change to drizzle.config.js

--- apps/server/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./src/modules/*/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.MP_DATABASE_URL!,
-  },
-});
