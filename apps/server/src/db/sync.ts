c75079c400c07e1f5bdece18f9d34c814b0346af
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-01 13:29:46 +0200

    fix: remove verbose logging

--- apps/server/src/db/sync.ts
+++ apps/server/src/db/sync.ts
@@ -25,7 +25,6 @@ export function createDBSync({
 
         const state = getSnapshot();
 
-        logger.info("Saving state...");
         try {
           await db.transaction((tx) =>
             Promise.all([
