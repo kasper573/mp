7a23a6d26013fa03701f9d7616981961c92e1a5b
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-08 20:22:57 +0200

    fix: read vite mode properly

--- apps/client/src/env.ts
+++ apps/client/src/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  mode: import.meta.env.NODE_ENV as string,
+  mode: import.meta.env.MODE,
   serverUrl: (import.meta.env.MP_SERVER_URL as string) || "ws://localhost:4000",
 };
