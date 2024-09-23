0fc5b5b8f20c8377a3ecbea7df0bac8cb7f25dcc
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-23 05:59:32 +0200

    fix: loading component

---
+++ apps/client/src/ui/Loading.css.ts
@@ -0,0 +1,66 @@
+import { style, keyframes } from "@mp/style";
+
+const left = keyframes({
+  "0%": { transform: "scale(0)" },
+  "100%": { transform: "scale(1)" },
+});
+
+const mid = keyframes({
+  "0%": { transform: "translate(0, 0)" },
+  "100%": { transform: "translate(24px, 0)" },
+});
+
+const right = keyframes({
+  "0%": { transform: "scale(1)" },
+  "100%": { transform: "scale(0)" },
+});
+
+export const container = style({
+  display: "inline-block",
+  position: "relative",
+  width: "80px",
+  height: "80px",
+});
+
+const dot = style({
+  boxSizing: "border-box",
+  position: "absolute",
+  top: "33.33333px",
+  width: "13.33333px",
+  height: "13.33333px",
+  borderRadius: "50%",
+  background: "currentColor",
+  animationTimingFunction: "cubic-bezier(0, 1, 1, 0)",
+});
+
+export const dot1 = style([
+  dot,
+  {
+    left: "8px",
+    animation: `${left} 0.6s infinite`,
+  },
+]);
+
+export const dot2 = style([
+  dot,
+  {
+    left: "8px",
+    animation: `${mid} 0.6s infinite`,
+  },
+]);
+
+export const dot3 = style([
+  dot,
+  {
+    left: "32px",
+    animation: `${mid} 0.6s infinite`,
+  },
+]);
+
+export const dot4 = style([
+  dot,
+  {
+    left: "56px",
+    animation: `${right} 0.6s infinite`,
+  },
+]);
