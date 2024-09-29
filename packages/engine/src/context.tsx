5488bf8ed5db90099fa370b385ded44dd320ec1c
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-30 01:12:30 +0200

    chore: refactor context.tsx to use Show to force rerender

--- packages/engine/src/context.tsx
+++ packages/engine/src/context.tsx
@@ -1,5 +1,5 @@
-import type { JSX, ParentProps } from "solid-js";
-import { createContext, createMemo, onCleanup } from "solid-js";
+import type { ParentProps } from "solid-js";
+import { createContext, createMemo, onCleanup, Show } from "solid-js";
 import { Engine } from "./engine";
 
 export const EngineContext = createContext<Engine>(
@@ -19,24 +19,17 @@ export function EngineProvider(props: ParentProps<{ viewport: HTMLElement }>) {
   });
 
   return (
-    <ForceRerender signal={engine}>
+    // We use a keyed Show component to force re-render so that EngineContext does not have to provide a reactive value.
+    // This is a lesser-evil trade-off to avoid having to write reactive engine code in all our
+    // business logic when in practice the engine instance is basically never re-created.
+    // With a forced re-render, in the off chance that the engine instance would be re-created,
+    // the app will instead re-render.
+    <Show when={engine()} keyed>
       {(engine) => (
         <EngineContext.Provider value={engine}>
           {props.children}
         </EngineContext.Provider>
       )}
-    </ForceRerender>
+    </Show>
   );
-}
-
-// We force re-render so that EngineContext does not have to provide a reactive value.
-// This is a lesser-evil trade-off to avoid having to write reactive engine code in all our
-// business logic when in practice the engine instance is basically never re-created.
-// With a forced re-render, in the off chance that the engine instance would be re-created,
-// the app will instead re-render.
-function ForceRerender<T>(props: {
-  signal: () => T;
-  children: (instance: T) => JSX.Element;
-}) {
-  return <>{props.children(props.signal())}</>;
 }
