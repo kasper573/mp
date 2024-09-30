ae8a37cdeca46bd9c16a43e373c86e415c11a46e
Kasper <805006+kasper573@users.noreply.github.com>
2024-09-30 02:06:39 +0200

    fix: make viewport resizing work correctly

--- packages/solid-pixi/src/context.ts
+++ packages/solid-pixi/src/context.ts
@@ -1,18 +1,10 @@
-import type { Container, Application } from "@mp/pixi";
+import type { Container } from "@mp/pixi";
 import { createContext } from "solid-js";
 
 export const ParentContext = createContext<Container>(
   new Proxy({} as Container, {
     get() {
       throw new Error("ParentContext not provided");
-    },
-  }),
-);
-
-export const ApplicationContext = createContext<Application>(
-  new Proxy({} as Application, {
-    get() {
-      throw new Error("ApplicationContext not provided");
     },
   }),
 );
