92cb4d4090218cb8881bc45b3632f844df966a38
Kasper <nonyabusiness@whatever.com>
2024-11-02 18:48:10 +0100

    fix: add top level await to vite because automerge requires it

--- packages/build/vite.mjs
+++ packages/build/vite.mjs
@@ -3,6 +3,7 @@ import { checker } from "vite-plugin-checker";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import solid from "vite-plugin-solid";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 /**
  * @param {string} projectRoot
@@ -13,6 +14,7 @@ export function defineConfig(projectRoot) {
       vanillaExtractPlugin(),
       solid(),
       wasm(),
+      topLevelAwait(),
       checker({ typescript: true }),
     ],
     envPrefix: "MP_",
