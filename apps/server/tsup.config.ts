86c7e3669720cf194a32f548a8eec3872422ff79
Kasper <nonyabusiness@whatever.com>
2024-11-03 19:03:58 +0100

    fix: separate build config for server in prod

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -4,4 +4,17 @@ export default defineConfig({
   entry: { index: "src/main.ts" },
   // Nothing needs the typesecript declaration files of the server app
   dts: false,
+  // Building for production means bundling everything into a docker image,
+  // but we don't want to have to do this in development for all changes,
+  // so we only do it in production.
+  ...(process.env.PROD
+    ? {
+        outExtension: () => ({ js: `.js` }),
+        noExternal: [/.*/],
+        format: "cjs",
+        target: "node20",
+        bundle: true,
+        splitting: false,
+      }
+    : {}),
 });
