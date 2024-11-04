ee5ed80a08d316fa0b398cc484b919b631279d48
Kasper <nonyabusiness@whatever.com>
2024-11-04 01:39:22 +0100

    fix: remove immer from state package

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -1,7 +1,4 @@
 import { createSignal, type Setter, type Accessor } from "solid-js";
-import { enableMapSet } from "immer";
-
-enableMapSet();
 
 export * from "neverthrow";
 export { createStore, type Store } from "solid-js/store";
@@ -12,7 +9,6 @@ export function atom<T>(initial: T): Atom<T> {
 }
 
 export { createMemo as computed, batch } from "solid-js";
-export { produce, type Patch } from "immer";
 
 export interface Atom<T> {
   get: Accessor<T>;
