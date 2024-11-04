7d0d14d6abb498aa567d298171c56327684d248a
Kasper <nonyabusiness@whatever.com>
2024-11-04 01:56:52 +0100

    fix: refactor client lifecycle

--- packages/sync/src/client.ts
+++ packages/sync/src/client.ts
@@ -8,7 +8,7 @@ export class SyncClient<State> {
   private wsAdapter?: BrowserWebSocketClientAdapter;
   private repo?: Repo;
   private handle?: DocHandle<State>;
-  private subscriptions = new Set<SyncClientSubscription<State>>();
+  private stateSubscriptions = new Set<SyncClientSubscription<State>>();
 
   readonly clientId: ClientId;
 
@@ -21,39 +21,40 @@ export class SyncClient<State> {
   }
 
   start() {
+    if (this.repo) {
+      throw new Error("Cannot start a client that has already started");
+    }
     this.wsAdapter = new BrowserWebSocketClientAdapter(this.url);
-    this.repo = new Repo({
-      network: [this.wsAdapter],
-      peerId: this.clientId,
-    });
+    this.repo = new Repo({ network: [this.wsAdapter], peerId: this.clientId });
     this.repo.on("document", this.acceptDocument);
   }
 
   stop() {
-    if (!this.handle || !this.wsAdapter || !this.repo) {
+    if (!this.repo) {
       throw new Error("Cannot stop a client that hasn't started");
     }
 
-    this.wsAdapter.disconnect();
-    this.handle.off("change");
+    this.wsAdapter?.disconnect();
+    this.handle?.off("change");
+    this.repo.off("document");
     this.wsAdapter = undefined;
     this.repo = undefined;
-    this.handle = undefined;
   }
 
   subscribe = (handler: SyncClientSubscription<State>) => {
-    this.subscriptions.add(handler);
-    return () => this.subscriptions.delete(handler);
+    this.stateSubscriptions.add(handler);
+    return () => this.stateSubscriptions.delete(handler);
   };
 
   private acceptDocument = ({ handle }: DocumentPayload) => {
+    this.handle?.off("change");
     this.handle = handle;
-    this.handle.on("change", this.emitCurrentDocument);
-    void this.handle.doc().then(this.emitCurrentDocument);
+    this.handle.on("change", this.emitState);
+    void this.handle.doc().then(this.emitState);
   };
 
-  private emitCurrentDocument = () => {
-    for (const handler of this.subscriptions) {
+  private emitState = () => {
+    for (const handler of this.stateSubscriptions) {
       handler(this.getState());
     }
   };
