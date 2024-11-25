443ed792790a8584b7b8ae0dc1fe5cea9f301376
Kasper <805006+kasper573@users.noreply.github.com>
2024-11-25 05:04:54 +0100

    fix: correct http error for auth middleware

--- apps/server/src/middlewares/auth.ts
+++ apps/server/src/middlewares/auth.ts
@@ -1,15 +1,19 @@
+import { TRPCError } from "@trpc/server";
 import { t } from "../trpc";
 
 export function auth() {
   return t.middleware(async ({ ctx, next }) => {
     const { authToken, auth } = ctx;
     if (!authToken) {
-      throw new Error(`Client provided no auth token`);
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "No token provided",
+      });
     }
 
     const result = await auth.verifyToken(authToken);
     if (!result.ok) {
-      throw new Error(`Client failed to authenticate: ${result.error}`);
+      throw new TRPCError({ code: "UNAUTHORIZED", message: result.error });
     }
 
     return next({ ctx: { ...ctx, user: result.user } });
