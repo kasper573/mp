1131dda5ea69e47d2c45e5ede8d2bc8e8d500b2f
Kasper <805006+kasper573@users.noreply.github.com>
2024-11-30 17:43:49 +0100

    chore: remove more debug logs

--- apps/server/drizzle.config.js
+++ apps/server/drizzle.config.js
@@ -1,11 +1,6 @@
 // Important: Do not import anything in this file,
 // since it's uploaded isolated to the server as part of deployment
 
-console.log(
-  "drizzle.config.js with MP_DATABASE_URL:",
-  process.env.MP_DATABASE_URL,
-);
-
 /** @type { import("drizzle-kit").Config } */
 export default {
   schema: "./src/modules/*/schema.ts",
