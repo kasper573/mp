86103e6b5e91cf57982b3efd4afc595f08a51d3d
Kasper <805006+kasper573@users.noreply.github.com>
2024-11-30 19:22:04 +0100

    ci: github reporter for playwright, updated artifacts config

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -10,18 +10,30 @@ dotenv.config({
 });
 
 const baseURL = `https://${process.env.MP_DOMAIN}`;
+const outputDir = ".playwright"; // Same value should also be defined in .gitignore
+const artifactsDir = path.join(outputDir, "artifacts");
+const snapshotDir = path.join(outputDir, "snapshots");
+const reportDir = path.join(outputDir, "report");
+
+const htmlReporter = [
+  "html",
+  { outputFolder: reportDir, open: "never" },
+] as const;
 
 export default defineConfig({
+  outputDir: artifactsDir,
+  snapshotDir,
   testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "dot" : "list",
+  reporter: process.env.CI ? [htmlReporter, ["github"]] : [htmlReporter],
   use: {
     baseURL,
-    trace: "on-first-retry",
-    video: "on-first-retry",
+    trace: "retain-on-failure",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     ignoreHTTPSErrors: true,
   },
 
