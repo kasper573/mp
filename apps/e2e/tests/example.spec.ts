367670c3542590aa0deb5fd32984bea46623be34
Kasper <805006+kasper573@users.noreply.github.com>
2024-11-30 19:22:12 +0100

    ci: add a failing test

--- apps/e2e/tests/example.spec.ts
+++ apps/e2e/tests/example.spec.ts
@@ -4,6 +4,6 @@ test("can load website", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle(/MP/);
-  const text = page.getByText(/Welcome to the home page/);
+  const text = page.getByText(/I am going to fail the test/);
   await expect(text).toBeVisible();
 });
