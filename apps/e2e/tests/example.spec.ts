9085e7ea678578fa8a8f7ea75f28cfcf5e817766
Kasper <805006+kasper573@users.noreply.github.com>
2024-11-30 20:03:37 +0100

    ci: fix the e2e test

--- apps/e2e/tests/example.spec.ts
+++ apps/e2e/tests/example.spec.ts
@@ -4,6 +4,6 @@ test("can load website", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle(/MP/);
-  const text = page.getByText(/I am going to fail the test/);
+  const text = page.getByText(/Welcome to the home page/);
   await expect(text).toBeVisible();
 });
