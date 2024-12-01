f253ae350069fd7cb371fce9a17c5a4dc0bf2134
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-01 15:15:59 +0100

    feat: new env var parsing convention in new package @mp/env

---
+++ packages/env/src/schemas.ts
@@ -0,0 +1,63 @@
+import type { BaseIssue, BaseSchema } from "valibot";
+import {
+  boolean,
+  number,
+  custom,
+  pipe,
+  string,
+  transform,
+  union,
+  parse,
+} from "valibot";
+
+export function boolish() {
+  return union([boolean(), booleanString()]);
+}
+
+export function numeric() {
+  return union([number(), numericString()]);
+}
+
+export function csv<
+  const Schema extends BaseSchema<unknown, unknown, BaseIssue<unknown>>,
+>(schema: Schema) {
+  return pipe(
+    string(),
+    transform((value) => value.split(",").map((v) => parse(schema, v.trim()))),
+  );
+}
+
+export function numericString() {
+  return pipe(
+    string(),
+    custom((value) => parseNumericString(String(value)) !== undefined),
+    transform((value) => parseNumericString(value)!),
+  );
+}
+
+export function booleanString() {
+  return pipe(
+    string(),
+    custom((value) => {
+      const lcs = String(value).toLowerCase();
+      return yesValues.has(lcs) || noValues.has(lcs);
+    }),
+    transform((value) => {
+      const lcs = String(value).toLowerCase();
+      return yesValues.has(lcs);
+    }),
+  );
+}
+
+function parseNumericString(value: string): number | undefined {
+  if (/^\d+$/.test(value)) {
+    return Number.parseInt(value, 10);
+  }
+  if (/^\d+\.\d+$/.test(value)) {
+    return Number.parseFloat(value);
+  }
+}
+
+const yesValues = new Set(["true", "1", "yes", "on"]);
+
+const noValues = new Set(["false", "0", "no", "off"]);
