6ac743b3c151715eb6220e4e02817e17c0cdd598
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-08 19:36:59 +0100

    chore: deno.json for @mp/client

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,37 +1,43 @@
-import { clientEnvGlobalVarName, clientEnvSchema } from "@mp/server";
-import { parseEnv } from "@mp/env";
-import type { Plugin } from "vite";
 import { defineConfig } from "vite";
-import { checker } from "vite-plugin-checker";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import deno from "@deno/vite-plugin";
 import solid from "vite-plugin-solid";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
-import process from "node:process";
+//import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vanillaExtractPlugin(),
-    solid(),
-    wasm(),
-    topLevelAwait(),
-    checker({ typescript: true }),
-    ...(process.env.MP_BUNDLE_CLIENT_ENV ? [clientEnvPlugin()] : []),
-  ],
+  plugins: [deno(), solid()],
 });
 
-function clientEnvPlugin(): Plugin {
-  const res = parseEnv(clientEnvSchema, process.env, "MP_CLIENT_");
-  if (res.isErr()) {
-    throw new Error("Failed to parse client env:\n" + res.error);
-  }
-  return {
-    name: "vite-plugin-mp-client-env",
-    transformIndexHtml(html) {
-      return html.replaceAll(
-        "__WILL_BE_REPLACED_WITH_ENV_VARS_SCRIPT__",
-        `window["${clientEnvGlobalVarName}"] = ${JSON.stringify(res.value)};`
-      );
-    },
-  };
-}
+// import { clientEnvGlobalVarName, clientEnvSchema } from "@mp/server";
+// import { parseEnv } from "@mp/env";
+// import type { Plugin } from "vite";
+// import { defineConfig } from "vite";
+// import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+// import process from "node:process";
+// import deno from "@deno/vite-plugin";
+// import solid from "vite-plugin-solid";
+
+// export default defineConfig({
+//   plugins: [
+//     deno(),
+//     vanillaExtractPlugin(),
+//     solid(),
+//     ...(process.env.MP_BUNDLE_CLIENT_ENV ? [clientEnvPlugin()] : []),
+//   ],
+// });
+
+// function clientEnvPlugin(): Plugin {
+//   const res = parseEnv(clientEnvSchema, process.env, "MP_CLIENT_");
+//   if (res.isErr()) {
+//     throw new Error("Failed to parse client env:\n" + res.error);
+//   }
+//   return {
+//     name: "vite-plugin-mp-client-env",
+//     transformIndexHtml(html) {
+//       return html.replaceAll(
+//         "__WILL_BE_REPLACED_WITH_ENV_VARS_SCRIPT__",
+//         `window["${clientEnvGlobalVarName}"] = ${JSON.stringify(res.value)};`
+//       );
+//     },
+//   };
+// }
