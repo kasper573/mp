c8b985810c434b0474409dbaaf39c90605376098
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-13 20:09:56 +0100

    fix: enable client env vite script in development

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,8 +5,11 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
-// https://vite.dev/config/
-export default defineConfig({
+import { clientEnvGlobalVarName, clientEnvSchema } from "@mp/server";
+import { parseEnv } from "@mp/env";
+import type { Plugin } from "vite";
+
+export default defineConfig(({ mode }) => ({
   plugins: [
     deno(),
     solid(),
@@ -15,39 +18,22 @@ export default defineConfig({
     // @ts-expect-error Poor typedefs in package causes not callable error
     topLevelAwait(),
     vanillaExtractPlugin(),
+    ...(mode === "development" ? [clientEnvPlugin()] : []),
   ],
-});
+}));
 
-// import { clientEnvGlobalVarName, clientEnvSchema } from "@mp/server";
-// import { parseEnv } from "@mp/env";
-// import type { Plugin } from "vite";
-// import { defineConfig } from "vite";
-// import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-// import process from "node:process";
-// import deno from "@deno/vite-plugin";
-// import solid from "vite-plugin-solid";
-
-// export default defineConfig({
-//   plugins: [
-//     deno(),
-//     vanillaExtractPlugin(),
-//     solid(),
-//     ...(Deno.env("MP_BUNDLE_CLIENT_ENV") ? [clientEnvPlugin()] : []),
-//   ],
-// });
-
-// function clientEnvPlugin(): Plugin {
-//   const res = parseEnv(clientEnvSchema, Deno.env.toObject(), "MP_CLIENT_");
-//   if (res.isErr()) {
-//     throw new Error("Failed to parse client env:\n" + res.error);
-//   }
-//   return {
-//     name: "vite-plugin-mp-client-env",
-//     transformIndexHtml(html) {
-//       return html.replaceAll(
-//         "__WILL_BE_REPLACED_WITH_ENV_VARS_SCRIPT__",
-//         `window["${clientEnvGlobalVarName}"] = ${JSON.stringify(res.value)};`
-//       );
-//     },
-//   };
-// }
+function clientEnvPlugin(): Plugin {
+  const res = parseEnv(clientEnvSchema, Deno.env.toObject(), "MP_CLIENT_");
+  if (res.isErr()) {
+    throw new Error("Failed to parse client env:\n" + res.error);
+  }
+  return {
+    name: "vite-plugin-mp-client-env",
+    transformIndexHtml(html) {
+      return html.replaceAll(
+        "__WILL_BE_REPLACED_WITH_ENV_VARS_SCRIPT__",
+        `window["${clientEnvGlobalVarName}"] = ${JSON.stringify(res.value)};`,
+      );
+    },
+  };
+}
