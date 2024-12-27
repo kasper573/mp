0e17d8016311f6a3f76195244a6e0226ec752f72
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-27 05:34:53 +0100

    refactor: remove unnecessary function

--- packages/metrics/faroLogger.ts
+++ packages/metrics/faroLogger.ts
@@ -1,5 +1,5 @@
 import type { Faro } from "@grafana/faro-web-sdk";
-import { LogLevel, isError } from "@grafana/faro-web-sdk";
+import { LogLevel } from "@grafana/faro-web-sdk";
 import type { LoggerEventHandler } from "@mp/logger";
 
 /**
@@ -9,15 +9,11 @@ export function faroLoggerHandler(faro: Faro): LoggerEventHandler {
   return (entry) => {
     switch (entry.type) {
       case "error":
-        return faro.api.pushError(ensureError(entry.error));
+        return faro.api.pushError(entry.error);
       case "warn":
         return faro.api.pushLog(entry.args, { level: LogLevel.WARN });
       case "info":
         return faro.api.pushLog(entry.args, { level: LogLevel.INFO });
     }
   };
-}
-
-function ensureError(e: unknown): Error {
-  return isError(e) ? e : new Error(String(e));
 }
