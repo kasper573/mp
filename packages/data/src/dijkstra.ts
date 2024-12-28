becdc92acbcdba0ae5c64ef5c5d736e9bf0f322c
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-28 11:44:38 +0100

    fix: make dijkstra module compile in tsx

--- packages/data/src/dijkstra.ts
+++ packages/data/src/dijkstra.ts
@@ -1,6 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-export { find_path } from "dijkstrajs";
+import dijkstrajs from "dijkstrajs";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+export const find_path = dijkstrajs.find_path;
 
 // For some reason typescript refuses to compile this file in vite even if I define
 // the proper types as a declaration file, so we have to use ts-nocheck.
