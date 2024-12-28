e0b8c5e1886d358f5b944981a3432948d14cb6d6
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-28 11:47:37 +0100

    fix: import pg properly so that it can run in tsx

--- apps/server/src/db/client.ts
+++ apps/server/src/db/client.ts
@@ -1,9 +1,9 @@
-import { Pool } from "pg";
+import pg from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 
 export type DBClient = ReturnType<typeof createDBClient>;
 
 export function createDBClient(connectionString: string) {
-  const pool = new Pool({ connectionString });
+  const pool = new pg.Pool({ connectionString });
   return drizzle({ client: pool });
 }
