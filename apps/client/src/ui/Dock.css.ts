6fc3bdd5e0884f531071801c5e1f659c8f2a8ca9
Kasper <805006+kasper573@users.noreply.github.com>
2025-01-06 09:59:22 +0100

    fix: create Dock component

---
+++ apps/client/src/ui/Dock.css.ts
@@ -0,0 +1,15 @@
+import { atoms, recipe } from "@mp/style";
+
+export const dock = recipe({
+  base: {
+    position: "absolute",
+  },
+  variants: {
+    position: {
+      center: [
+        atoms({ top: "50%", left: "50%" }),
+        { transform: "translate(-50%, -50%)" },
+      ],
+    },
+  },
+});
