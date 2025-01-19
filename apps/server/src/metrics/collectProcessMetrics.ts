ffb1e6b0d55a4d460c603c47056e811d9565921f
Kasper <805006+kasper573@users.noreply.github.com>
2025-01-19 02:11:46 +0100

    feat: add metrics for npc count

--- apps/server/src/metrics/collectProcessMetrics.ts
+++ apps/server/src/metrics/collectProcessMetrics.ts
@@ -1,39 +1,13 @@
 import type { MetricsRegistry } from "@mp/telemetry/prom";
 import { MetricsGague } from "@mp/telemetry/prom";
-import type { WorldSyncServer } from "../modules/world/WorldState";
-import type { ClientRegistry } from "../ClientRegistry";
 
-export function collectUserMetrics(
-  registry: MetricsRegistry,
-  clients: ClientRegistry,
-  worldState: WorldSyncServer,
-) {
+export function collectProcessMetrics(metrics: MetricsRegistry) {
   new MetricsGague({
-    name: "active_user_count",
-    help: "Number of users currently connected",
-    registers: [registry],
+    name: "process_uptime_seconds",
+    help: "Time since the process started in seconds",
+    registers: [metrics],
     collect() {
-      this.set(clients.getUserCount());
-    },
-  });
-
-  new MetricsGague({
-    name: "active_character_count",
-    help: "Number of player characters currently active",
-    registers: [registry],
-    collect() {
-      worldState.access("collectUserMetrics", (state) => {
-        this.set(Object.values(state.characters).length);
-      });
-    },
-  });
-
-  new MetricsGague({
-    name: "active_client_count",
-    help: "Number of active websocket connections",
-    registers: [registry],
-    collect() {
-      this.set([...worldState.clientIds].length);
+      this.set(process.uptime());
     },
   });
 }
--- apps/server/src/metrics/collectUserMetrics.ts
+++ apps/server/src/metrics/collectUserMetrics.ts
@@ -1,13 +1,53 @@
 import type { MetricsRegistry } from "@mp/telemetry/prom";
 import { MetricsGague } from "@mp/telemetry/prom";
+import type { WorldSyncServer } from "../modules/world/WorldState";
+import type { ClientRegistry } from "../ClientRegistry";
 
-export function collectProcessMetrics(metrics: MetricsRegistry) {
+export function collectUserMetrics(
+  registry: MetricsRegistry,
+  clients: ClientRegistry,
+  worldState: WorldSyncServer,
+) {
   new MetricsGague({
-    name: "process_uptime_seconds",
-    help: "Time since the process started in seconds",
-    registers: [metrics],
+    name: "active_user_count",
+    help: "Number of users currently connected",
+    registers: [registry],
     collect() {
-      this.set(process.uptime());
+      this.set(clients.getUserCount());
+    },
+  });
+
+  new MetricsGague({
+    name: "active_character_count",
+    help: "Number of player characters currently active",
+    registers: [registry],
+    collect() {
+      worldState.access(
+        "collectUserMetrics.active_character_count",
+        (state) => {
+          this.set(Object.values(state.characters).length);
+        },
+      );
+    },
+  });
+
+  new MetricsGague({
+    name: "active_npc_count",
+    help: "Number of non-player characters currently active",
+    registers: [registry],
+    collect() {
+      worldState.access("collectUserMetrics.active_npc_count", (state) => {
+        this.set(Object.values(state.npcs).length);
+      });
+    },
+  });
+
+  new MetricsGague({
+    name: "active_client_count",
+    help: "Number of active websocket connections",
+    registers: [registry],
+    collect() {
+      this.set([...worldState.clientIds].length);
     },
   });
 }
