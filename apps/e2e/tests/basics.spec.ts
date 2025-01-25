f41030b4e8137f244058bd81494bc933454ed199
Kasper <805006+kasper573@users.noreply.github.com>
2025-01-26 00:48:00 +0100

    fix: update the test to test reality

--- apps/e2e/tests/basics.spec.ts
+++ apps/e2e/tests/basics.spec.ts
@@ -4,6 +4,6 @@ test("can load website", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle(/MP/);
-  const text = page.getByText(/Welcome to the home page/);
+  const text = page.getByText(/Welcome/i);
   await expect(text).toBeVisible();
 });
