525880fff2d61d55484e594e67ce6057cf95efce
Kasper <805006+kasper573@users.noreply.github.com>
2025-02-01 22:16:21 +0100

    refactor: break some code into a separate component

--- apps/client/src/pages/game/GamePage.tsx
+++ apps/client/src/pages/game/GamePage.tsx
@@ -2,30 +2,28 @@ import {
   createEffect,
   Match,
   onCleanup,
-  Show,
-  Suspense,
   Switch,
   useContext,
+  Show,
 } from "solid-js";
 import { EngineContext, EngineProvider } from "@mp/engine";
 import { AuthContext } from "@mp/auth/client";
 import { Application } from "@mp/solid-pixi";
+import type { AreaResource } from "@mp/data";
 import { createSyncClient, SyncClientContext } from "../../integrations/sync";
 import { useAreaResource } from "../../state/useAreaResource";
 import { Dock } from "../../ui/Dock";
 import { LoadingSpinner } from "../../ui/LoadingSpinner";
 import { toggleSignal } from "../../state/toggleSignal";
 import * as styles from "./GamePage.css";
+import { AreaDebugUI } from "./AreaDebugUI";
 import { AreaScene } from "./AreaScene";
 import { WorldStateInspector } from "./WorldStateInspector";
-import { AreaDebugUI } from "./AreaDebugUI";
 
 export default function GamePage() {
   const auth = useContext(AuthContext);
   const world = createSyncClient(auth);
   const area = useAreaResource(world.areaId);
-
-  const [debug, toggleDebug] = toggleSignal();
 
   createEffect(() => {
     if (world.readyState() === "open") {
@@ -35,18 +33,6 @@ export default function GamePage() {
 
   return (
     <SyncClientContext.Provider value={world}>
-      <pre>
-        Data:{" "}
-        {JSON.stringify(
-          {
-            data: area.latest?.id ?? null,
-            loading: area.loading,
-            error: (area.error as unknown) ?? null,
-          },
-          null,
-          2,
-        )}
-      </pre>
       <Switch>
         <Match when={!auth.isSignedIn()}>
           <Dock position="center">Sign in to play</Dock>
@@ -56,33 +42,36 @@ export default function GamePage() {
           <LoadingSpinner debugId="GamePage" />
         </Match>
         <Match when={area.data} keyed>
-          {(data) => (
-            <Application class={styles.container}>
-              {({ viewport }) => (
-                <EngineProvider viewport={viewport}>
-                  <EngineBindings toggleDebug={toggleDebug} />
-                  <Suspense fallback="AreaScene loading...">
-                    <AreaScene area={data}>
-                      <Show when={debug()}>
-                        <AreaDebugUI
-                          area={data}
-                          pathsToDraw={world
-                            .actorsInArea()
-                            .flatMap((actor) =>
-                              actor.path ? [actor.path] : [],
-                            )}
-                        />
-                        <WorldStateInspector worldState={world.worldState()} />
-                      </Show>
-                    </AreaScene>
-                  </Suspense>
-                </EngineProvider>
-              )}
-            </Application>
-          )}
+          {(data) => <Game area={data} />}
         </Match>
       </Switch>
     </SyncClientContext.Provider>
+  );
+}
+
+function Game(props: { area: AreaResource }) {
+  const [debug, toggleDebug] = toggleSignal();
+  const world = useContext(SyncClientContext);
+  return (
+    <Application class={styles.container}>
+      {({ viewport }) => (
+        <EngineProvider viewport={viewport}>
+          <EngineBindings toggleDebug={toggleDebug} />
+
+          <AreaScene area={props.area}>
+            <Show when={debug()}>
+              <AreaDebugUI
+                area={props.area}
+                pathsToDraw={world
+                  .actorsInArea()
+                  .flatMap((actor) => (actor.path ? [actor.path] : []))}
+              />
+              <WorldStateInspector worldState={world.worldState()} />
+            </Show>
+          </AreaScene>
+        </EngineProvider>
+      )}
+    </Application>
   );
 }
 
