6091d4218cbc67e78926e1f2301c9c4016612cf6
Kasper <805006+kasper573@users.noreply.github.com>
2025-02-23 21:56:17 +0100

    fix: adjust basic wload website e2e test to align with new changes

--- apps/e2e/tests/basics.spec.ts
+++ apps/e2e/tests/basics.spec.ts
@@ -4,6 +4,6 @@ test("can load website", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle(/MP/);
-  const text = page.getByText(/Welcome/i);
+  const text = page.getByText(/Play the game/i);
   await expect(text).toBeVisible();
 });
