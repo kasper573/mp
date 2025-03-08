62397f5cfd243ec60b1d1609834b44b862bf5403
Kasper <805006+kasper573@users.noreply.github.com>
2025-03-08 23:54:07 +0100

    fix: drizzle schema composition

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,6 +1,6 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-  schema: "./src/modules/*/schema.ts",
+  schema: "./src/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.MP_SERVER_DATABASE_URL,
