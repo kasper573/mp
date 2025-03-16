25b08bd9506e55fb2f53f8c2195eeecb544d85e9
Kasper <805006+kasper573@users.noreply.github.com>
2025-03-16 02:13:23 +0100

    refactor: add function composition to mp/std

--- libraries/std/src/index.ts
+++ libraries/std/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./types";
 export * from "./random";
 export * from "./iterators";
+export * from "./functionComposition";
 export * from "neverthrow";
 export { v7 as uuid } from "uuid";
