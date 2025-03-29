59c921181fb7e2da0ee9247c4a4271b2a4baa7d3
Kasper <805006+kasper573@users.noreply.github.com>
2025-03-29 22:22:22 +0100

    fix: admin should have all player roles

--- apps/server/src/roles.ts
+++ apps/server/src/roles.ts
@@ -2,16 +2,18 @@ import { characterRoles, npcRoles } from "@mp-modules/game/server";
 
 export const playerGroup = "player";
 
+const playerRoles = [
+  characterRoles.join,
+  characterRoles.move,
+  characterRoles.attack,
+  characterRoles.respawn,
+];
+
 /**
  * This is the single source of truth of groups and roles.
  * Changing this will provision updates in keycloak when pushed to production.
  */
 export const groupedRoles = {
-  admin: [characterRoles.kill, npcRoles.spawnRandom],
-  [playerGroup]: [
-    characterRoles.join,
-    characterRoles.move,
-    characterRoles.attack,
-    characterRoles.respawn,
-  ],
+  admin: [...playerRoles, characterRoles.kill, npcRoles.spawnRandom],
+  [playerGroup]: playerRoles,
 };
