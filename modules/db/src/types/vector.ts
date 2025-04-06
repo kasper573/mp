6edfefbda790782b2c0237eab208d695073ff95c
Kasper <805006+kasper573@users.noreply.github.com>
2025-04-06 16:21:38 +0200

    fix: instantiate Vector from database schema

--- modules/db/src/types/vector.ts
+++ modules/db/src/types/vector.ts
@@ -1,9 +1,25 @@
-import type { Vector } from "@mp/math";
-import { point } from "drizzle-orm/pg-core";
+import { Vector } from "@mp/math";
+import { customType } from "drizzle-orm/pg-core";
 
 /**
  * A drizzle/postgres representation of the Vector type from @mp/std
  */
 export function vector<T extends number>() {
-  return point({ mode: "xy" }).$type<Vector<T>>();
+  const buildSchema = customType<{
+    data: Vector<T>;
+    driverData: [T, T];
+    notNull: true;
+  }>({
+    dataType() {
+      return "point";
+    },
+    toDriver(v) {
+      return [v.x, v.y];
+    },
+    fromDriver([x, y]) {
+      return new Vector<T>(x, y);
+    },
+  });
+
+  return buildSchema();
 }
