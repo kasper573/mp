8dea2194bd4825c5c02a9faaa6016e0995c06ac7
Kasper <805006+kasper573@users.noreply.github.com>
2025-04-06 17:17:43 +0200

    fix: custom vector column builder

--- modules/db/src/types/vector.ts
+++ modules/db/src/types/vector.ts
@@ -1,25 +1,34 @@
+import type { VectorLike } from "@mp/math";
 import { Vector } from "@mp/math";
-import { customType } from "drizzle-orm/pg-core";
+import type { Table } from "drizzle-orm";
+import { type ColumnBaseConfig } from "drizzle-orm";
+import { PgPointObject, PgPointObjectBuilder } from "drizzle-orm/pg-core";
 
 /**
  * A drizzle/postgres representation of the Vector type from @mp/std
  */
-export function vector<T extends number>() {
-  const buildSchema = customType<{
-    data: Vector<T>;
-    driverData: [T, T];
-    notNull: true;
-  }>({
-    dataType() {
-      return "point";
-    },
-    toDriver(v) {
-      return [v.x, v.y];
-    },
-    fromDriver([x, y]) {
-      return new Vector<T>(x, y);
-    },
-  });
+export function vector<T extends number>(name = "") {
+  return new VectorBuilder<T>(name);
+}
 
-  return buildSchema();
+class VectorObject<T extends number> extends PgPointObject<
+  VectorColumnConfig<T>
+> {
+  override mapFromDriverValue(value: VectorLike<number>) {
+    return Vector.from(value);
+  }
+}
+
+class VectorBuilder<T extends number> extends PgPointObjectBuilder<
+  VectorColumnConfig<T>
+> {
+  build(table: Table) {
+    return new VectorObject<T>(table, this.config);
+  }
+}
+
+interface VectorColumnConfig<T extends number>
+  extends ColumnBaseConfig<"json", "PgPointObject"> {
+  data: Vector<T>;
+  driverData: VectorLike<T>;
 }
