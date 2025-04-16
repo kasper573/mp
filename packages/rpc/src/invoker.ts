70e78115f23fcbf6da1651bbe312baf2056e061f
Kasper <805006+kasper573@users.noreply.github.com>
2025-04-16 03:17:41 +0200

    feat: implement rpc invoker

--- packages/rpc/src/invoker.ts
+++ packages/rpc/src/invoker.ts
@@ -1,35 +1,36 @@
-import { err } from "@mp/std";
-import type {
-  AnyRouteRecord,
-  AnyRPCNode,
-  MutationNode,
-  QueryNode,
-  RouterNode,
-} from "./builder";
+import { err, ok } from "@mp/std";
+import type { AnyRouteRecord, AnyRPCNode, RouterNode } from "./builder";
 import type { Invoker } from "./transmitter";
 
 export function createRPCInvoker<Input, Output, Context>(
   root: RouterNode<AnyRouteRecord<Context>>,
 ): Invoker<Input, Output, Context> {
   return async function invokeRPC(call, ctx) {
-    const [path, input, callId] = call;
-    const node = resolveProcedureNode<Input, Output, Context>(root, path);
-    if (!node) {
+    const [path, input] = call;
+    const node = resolveProcedureNode<Context>(root, path);
+    if (!node || node.type === "router") {
       return err({ type: "invalid-path" });
     }
 
-    const output = await node.handler({ ctx, input, mwc: undefined });
-
-    throw new Error("Not implemented");
+    try {
+      const mwc = undefined; // The first handler never has a middleware context
+      const output = (await node.handler({ ctx, input, mwc })) as Output;
+      return ok(output);
+    } catch (error) {
+      return err({ type: "exception", error });
+    }
   };
 }
 
-function resolveProcedureNode<Input, Output, Context>(
+function resolveProcedureNode<Context>(
   start: AnyRPCNode,
   path: string[],
-):
-  | QueryNode<Input, Output, Context, unknown>
-  | MutationNode<Input, Output, Context, unknown>
-  | undefined {
-  throw new Error("Not implemented");
+): AnyRPCNode<Context> | undefined {
+  let node: AnyRPCNode | undefined = start;
+  for (const key of path) {
+    if (node.type === "router") {
+      node = node.routes[key];
+    }
+  }
+  return node;
 }
