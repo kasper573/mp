58004aab04fe90b3bdf38d5b9c57ec22c3c6697a
Kasper <805006+kasper573@users.noreply.github.com>
2025-04-16 06:17:27 +0200

    fix: encode header properly

--- packages/encoding/src/index.ts
+++ packages/encoding/src/index.ts
@@ -5,18 +5,20 @@ export { addExtension as addEncoderExtension } from "cbor-x";
 export function createEncoding<T>(header: number) {
   return {
     decode(data: ArrayBufferLike): T | undefined {
-      const view = new Uint8Array(data);
-      if (view.at(0) !== header) {
+      const view = new DataView(data);
+      if (view.getUint16(0) !== header) {
         return;
       }
-      return cbor.decode(view.slice(1)) as T;
+      return cbor.decode(new Uint8Array(data, 2)) as T;
     },
     encode(value: T): ArrayBufferLike {
       const encodedValue = cbor.encode(value) as Uint8Array;
-      const result = new Uint8Array(encodedValue.byteLength + 1);
-      result[0] = header;
-      result.set(new Uint8Array(encodedValue), 1);
-      return result.buffer;
+      const buffer = new ArrayBuffer(encodedValue.byteLength + 2);
+
+      new DataView(buffer).setUint16(0, header);
+      new Uint8Array(buffer, 2).set(encodedValue);
+
+      return buffer;
     },
   };
 }
