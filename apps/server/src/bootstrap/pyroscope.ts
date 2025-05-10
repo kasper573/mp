5509d86b6def55b6d975a32ed1645fc0de9c2533
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-11 01:16:49 +0200

    refactor: remove unnecessary code

--- apps/server/src/bootstrap/pyroscope.ts
+++ apps/server/src/bootstrap/pyroscope.ts
@@ -1,8 +1,6 @@
 import * as Pyroscope from "@mp/telemetry/pyroscope";
 
 Pyroscope.init({
-  serverAddress: process.env.PYROSCOPE_SERVER_ADDRESS,
-  appName: process.env.PYROSCOPE_APPLICATION_NAME,
   wall: {
     collectCpuTime: true,
   },
