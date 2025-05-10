d660e9ab166c50786fafbc4ee6d107058aae31cd
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-11 00:18:21 +0200

    fix: add explicit server address and app name to pyroscope init

--- apps/server/src/bootstrap/pyroscope.ts
+++ apps/server/src/bootstrap/pyroscope.ts
@@ -1,6 +1,8 @@
 import * as Pyroscope from "@mp/telemetry/pyroscope";
 
 Pyroscope.init({
+  serverAddress: process.env.PYROSCOPE_SERVER_ADDRESS,
+  appName: process.env.PYROSCOPE_APPLICATION_NAME,
   wall: {
     collectCpuTime: true,
   },
