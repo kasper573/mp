86f6316a3c640a8710048d55cc52022d1f8bb1c8
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-18 23:34:59 +0200

    fix: rename debug ui to misc debug ui and add rpc integration for server tick control

--- apps/client/src/ui/misc-debug-ui.tsx
+++ apps/client/src/ui/misc-debug-ui.tsx
@@ -3,24 +3,40 @@ import {
   GameStateClientContext,
   type OptimisticGameStateSettings,
 } from "@mp/game/client";
-import { useContext } from "solid-js";
+import { createEffect, createSignal, useContext } from "solid-js";
 import { assert } from "@mp/std";
 import { createStorageSignal } from "@mp/state";
+import { TimeSpan } from "@mp/time";
 import { useRpc } from "../integrations/rpc";
 
-export function DebugUi() {
+export function MiscDebugUi() {
   const rpc = useRpc();
   const gameState = useContext(GameStateClientContext);
-  const [settings, setSettings] = createStorageSignal<DebugSettings>(
+  const [serverTick, setServerTick] = createServerTickSignal();
+  const [settings, setSettings] = createStorageSignal<MiscDebugSettings>(
     localStorage,
-    "debug-settings",
+    "misc-debug-settings",
     defaultSettings,
   );
 
   return (
     <>
       <div>
-        Server tick: 1ms <input type="range" min={1} max={1000} /> 1000ms
+        Server tick: 1ms{" "}
+        <input
+          type="range"
+          min={1}
+          max={1000}
+          value={serverTick().totalMilliseconds}
+          on:change={(e) =>
+            setServerTick(
+              TimeSpan.fromMilliseconds(
+                Number.parseInt(e.currentTarget.value, 10),
+              ),
+            )
+          }
+        />{" "}
+        1000ms
       </div>
       <div>
         Use client side patch optimizer:{" "}
@@ -49,21 +65,6 @@ export function DebugUi() {
         />
       </div>
       <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={settings().visualizeNetworkFogOfWar}
-            on:change={(e) =>
-              setSettings((prev) => ({
-                ...prev,
-                visualizeNetworkFogOfWar: e.currentTarget.checked,
-              }))
-            }
-          />
-          Visualize network fog of war
-        </label>
-      </div>
-      <div>
         <Button on:click={() => void rpc.npc.spawnRandomNpc()}>
           Spawn random NPC
         </Button>
@@ -81,12 +82,32 @@ export function DebugUi() {
   );
 }
 
-const defaultSettings: DebugSettings = {
+function createServerTickSignal() {
+  const rpc = useRpc();
+  const [serverTick, setServerTick] = createSignal(
+    TimeSpan.fromMilliseconds(50),
+  );
+  const remoteServerTick = rpc.system.serverTickInterval.useQuery();
+
+  createEffect(() => {
+    void rpc.system.setServerTickInterval(serverTick());
+  });
+
+  createEffect(() => {
+    if (remoteServerTick.data) {
+      setServerTick(remoteServerTick.data);
+    }
+  });
+
+  return [serverTick, setServerTick] as const;
+}
+
+const defaultSettings: MiscDebugSettings = {
   useInterpolator: true,
   usePatchOptimizer: true,
   visualizeNetworkFogOfWar: false,
 };
 
-interface DebugSettings extends OptimisticGameStateSettings {
+interface MiscDebugSettings extends OptimisticGameStateSettings {
   visualizeNetworkFogOfWar: boolean;
 }
