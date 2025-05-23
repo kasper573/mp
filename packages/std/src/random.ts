b26a012edc41e691a5ba78dc3b98ef96691ea297
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-24 00:11:23 +0200

    fix: add random number generator to mp/std

--- packages/std/src/random.ts
+++ packages/std/src/random.ts
@@ -1,12 +1,25 @@
-export function randomItem<T>(arr: readonly T[]): T | undefined {
-  return arr[Math.floor(Math.random() * arr.length)];
+import type { RNG } from "random";
+import { MathRandomRNG } from "random";
+
+export * from "random";
+
+export function randomItem<T>(
+  arr: readonly T[],
+  rng: RNG = defaultRng,
+): T | undefined {
+  return arr[Math.floor(rng.next() * arr.length)];
 }
 
-export function randomizeArray<T>(arr: readonly T[]): T[] {
+export function randomizeArray<T>(
+  arr: readonly T[],
+  rng: RNG = defaultRng,
+): T[] {
   const shuffled = [...arr];
   for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(rng.next() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
   return shuffled;
 }
+
+export const defaultRng = new MathRandomRNG();
