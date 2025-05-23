53697d484db438130aa289ad57bd445072524dc0
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-24 00:11:41 +0200

    fix: make it possible to idle indefinitely

--- apps/game/server/npc/ai-tasks/idle.ts
+++ apps/game/server/npc/ai-tasks/idle.ts
@@ -2,13 +2,13 @@ import type { TimeSpan } from "@mp/time";
 import type { Task, TaskInput } from "./task";
 
 export function createIdleTask(
-  endTime: TimeSpan,
-  nextTask: (input: TaskInput) => Task,
+  endTime?: TimeSpan,
+  nextTask?: (input: TaskInput) => Task,
 ): Task {
   return function idle(input) {
     const { npc, gameState, tick } = input;
-    if (tick.totalTimeElapsed.compareTo(endTime) > 0) {
-      return nextTask(input);
+    if (endTime && tick.totalTimeElapsed.compareTo(endTime) > 0) {
+      return nextTask ? nextTask(input) : idle;
     }
     if (npc.path || npc.moveTarget) {
       gameState.actors.update(npc.id, (update) => {
