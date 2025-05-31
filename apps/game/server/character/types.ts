6b7a6d3615b08972f35f4cbceee0f8a93300f360
Kasper <805006+kasper573@users.noreply.github.com>
2025-05-31 15:25:06 +0200

    refactor: remove some redundant interface fields

--- apps/game/server/character/types.ts
+++ apps/game/server/character/types.ts
@@ -1,25 +1,12 @@
-import type { Branded, Tile, TimesPerSecond } from "@mp/std";
-import type { Vector } from "@mp/math";
+import type { Branded } from "@mp/std";
 import type { UserId } from "@mp/auth";
 import type { MovementTrait } from "../traits/movement";
-import type { ActorModelId } from "../traits/appearance";
 import { type AppearanceTrait } from "../traits/appearance";
 import type { CombatTrait } from "../traits/combat";
-import type { AreaId } from "../../shared/area/area-id";
 
 export interface Character extends AppearanceTrait, MovementTrait, CombatTrait {
   id: CharacterId;
-  coords: Vector<Tile>;
-  areaId: AreaId;
-  speed: Tile;
   userId: UserId;
-  health: number;
-  maxHealth: number;
-  attackDamage: number;
-  attackSpeed: TimesPerSecond;
-  attackRange: Tile;
-  modelId: ActorModelId;
-  name: string;
   xp: number;
 }
 
