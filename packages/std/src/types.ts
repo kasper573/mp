1987c6d0d8caa9d91696998c67ed040964cfc18e
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-01 19:55:28 +0200

    fix: remove unused dependency and types

--- packages/std/src/types.ts
+++ packages/std/src/types.ts
@@ -1,5 +1,3 @@
-import type { WritableKeysOf, Simplify } from "type-fest";
-
 export type Branded<T, Name extends string> = T & { brand: Name };
 
 /**
@@ -17,20 +15,3 @@ export type LocalFile = Branded<string, "LocalFile">;
 export type PublicUrl = Branded<string, "PublicUrl">;
 
 export type TimesPerSecond = Branded<number, "TimesPerSecond">;
-
-export type MinimalInput<T> = TreatUndefinedAsOptional<WritableSubset<T>>;
-
-export type WritableSubset<T> = {
-  [K in WritableKeysOf<T>]: T[K];
-};
-
-export type TreatUndefinedAsOptional<T> = Simplify<
-  Partial<Pick<T, UndefinedKeys<T>>> & Pick<T, DefinedKeys<T>>
->;
-
-type UndefinedKeys<T> = RerverseConditionalKeys<T, undefined>;
-type DefinedKeys<T> = Exclude<keyof T, UndefinedKeys<T>>;
-
-type RerverseConditionalKeys<Base, Condition> = {
-  [K in keyof Base]: Condition extends Base[K] ? K : never;
-}[keyof Base];
