81b5a2c2c034203f255527bc517edeca9d146a4b
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-13 23:21:54 +0200

    fix: remove hack

--- apps/e2e/tests/game.spec.ts
+++ apps/e2e/tests/game.spec.ts
@@ -4,15 +4,6 @@ import { registerAndSignIn } from "../actions/register-and-sign-in";
 test("can see game canvas after signing in", async ({ page }) => {
   await registerAndSignIn(page);
 
-  // Tanstack router seems to have a bug where the lazy loading of routes get stuck in an infinite suspense state
-  // if you immediately navigate to some other route after the page has loaded.
-  // This can be reproduced by just clicking play really really fast after loading the page manually.
-  // There's no easy fix outside of fixing the issue in the router itself,
-  // so as a workaround we just wait for the network to be idle.
-  // We can pretend that this represents a patient user that is waiting for the page to load, so it's not terrible.
-  // TODO remove this workaround if the router issue ever becomes fixed.
-  await page.waitForLoadState("networkidle");
-
   await page.getByRole("link", { name: /play/i }).click();
 
   await expect(page.locator("canvas")).toBeVisible();
