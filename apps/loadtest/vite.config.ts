f70861c4869b34ef2ee9682d94fdf2a52deb514e
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-15 18:30:13 +0200

    fix: add vite config and vite-node to loadtest app

---
+++ apps/loadtest/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [vanillaExtractPlugin(), solid()],
+});
