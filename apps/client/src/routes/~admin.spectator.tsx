d4048547c4cd6d0723ac6163d205f2adaab385c5
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-20 19:10:08 +0200

    protect spectator page behind role boundary

--- apps/client/src/routes/~admin.spectator.tsx
+++ apps/client/src/routes/~admin.spectator.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/solid-router'
+import { createFileRoute } from "@tanstack/solid-router";
+import { worldRoles } from "@mp/game/client";
+import { AuthBoundary } from "../ui/auth-boundary";
 
-export const Route = createFileRoute('/admin/spectator')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/admin/spectator")({
+  component: () => (
+    <AuthBoundary requiredRoles={[worldRoles.spectate]}>
+      <RouteComponent />
+    </AuthBoundary>
+  ),
+});
 
 function RouteComponent() {
-  return <div>Hello "/admin/spectator"!</div>
+  return <div>Hello "/admin/spectator"!</div>;
 }
--- apps/client/src/ui/auth-boundary.tsx
+++ apps/client/src/ui/auth-boundary.tsx
@@ -1,15 +1,27 @@
 import { AuthContext } from "@mp/auth/client";
 import type { JSX, ParentProps } from "solid-js";
-import { useContext, Switch, Match } from "solid-js";
+import { useContext, Switch, Match, createMemo } from "solid-js";
+import type { RoleDefinition } from "@mp/auth";
 import PermissionDenied from "../routes/permission-denied";
 
-export function AuthBoundary(props: ParentProps): JSX.Element {
+export function AuthBoundary(
+  props: ParentProps<{ requiredRoles?: Iterable<RoleDefinition> }>,
+): JSX.Element {
   const auth = useContext(AuthContext);
+
+  const isPermitted = createMemo(() => {
+    if (!auth.isSignedIn()) {
+      return false;
+    }
+    const existingSet = auth.identity()?.roles ?? new Set();
+    const requiredSet = new Set(props.requiredRoles);
+    return requiredSet.isSubsetOf(existingSet);
+  });
 
   return (
     <Switch>
-      <Match when={auth.isSignedIn()}>{props.children}</Match>
-      <Match when={!auth.isSignedIn()}>
+      <Match when={isPermitted()}>{props.children}</Match>
+      <Match when={!isPermitted()}>
         <PermissionDenied />
       </Match>
     </Switch>
