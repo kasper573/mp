d3dda6f8001a424e20b189622d28689e67c2efd6
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-21 22:56:29 +0200

    call spectate rpc procedure when spectated character id changes

--- apps/game/client/spectator-client.tsx
+++ apps/game/client/spectator-client.tsx
@@ -1,21 +1,36 @@
 import type { SelectOption } from "@mp/ui";
 import { Select } from "@mp/ui";
-import { createSignal } from "solid-js";
+import { createEffect, createMemo, createSignal, useContext } from "solid-js";
+import { AuthContext } from "@mp/auth/client";
 import type { CharacterId } from "../server";
 import { useRpc } from "./use-rpc";
 import { Game } from "./game";
-import type { GameStateClient } from "./game-state-client";
+import { type GameStateClient } from "./game-state-client";
 
 export function SpectatorClient(props: { gameState: GameStateClient }) {
   const [spectatedCharacterId, setSpectatedCharacterId] =
     createSignal<CharacterId>();
   const rpc = useRpc();
+  const auth = useContext(AuthContext);
   const characterOptions = rpc.world.characterList.useQuery(() => ({
     input: void 0,
     refetchInterval: 5000,
     map: (result): SelectOption<CharacterId>[] =>
       result.items.map(({ id, name }) => ({ value: id, label: name })),
   }));
+
+  const isSocketOpen = createMemo(
+    () => props.gameState.readyState() === WebSocket.OPEN,
+  );
+
+  createEffect(() => {
+    const user = auth.identity();
+    const characterId = spectatedCharacterId();
+    if (isSocketOpen() && user && characterId) {
+      void rpc.world.spectate(characterId);
+    }
+  });
+
   return (
     <>
       <Select
