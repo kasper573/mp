507bdd7eb59958c0f6d3a4e079489bf65d5cd723
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-22 05:40:21 +0200

    center the permission denied screen

--- apps/client/src/routes/permission-denied.tsx
+++ apps/client/src/routes/permission-denied.tsx
@@ -6,7 +6,15 @@ export default function PermissionDenied() {
   const auth = useContext(AuthContext);
 
   return (
-    <div>
+    <div
+      style={{
+        position: "absolute",
+        top: "128px",
+        left: 0,
+        right: 0,
+        "text-align": "center",
+      }}
+    >
       <h1>Permission Denied</h1>
       <p>You must be signed in to access this page.</p>
       <Button onClick={() => void auth.redirectToSignIn()}>Sign in</Button>
