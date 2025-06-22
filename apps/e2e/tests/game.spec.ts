b7f7f893daac801b961945f3418d34c42f25afd1
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-22 06:17:51 +0200

    be more specific about with play link to click

--- apps/e2e/tests/game.spec.ts
+++ apps/e2e/tests/game.spec.ts
@@ -4,7 +4,10 @@ import { registerAndSignIn } from "../actions/register-and-sign-in";
 test("can see game canvas after signing in", async ({ page }) => {
   await registerAndSignIn(page);
 
-  await page.getByRole("link", { name: /play/i }).click();
+  await page
+    .getByRole("navigation")
+    .getByRole("link", { name: /play/i })
+    .click();
 
   await expect(page.locator("canvas")).toBeVisible();
 });
