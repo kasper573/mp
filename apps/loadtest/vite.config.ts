0c6385ae58953d11d5d25af5c1d7464b2b495c76
Kasper <805006+kasper573@users.noreply.github.com>
2025-06-29 09:28:42 +0200

    enable decorators in loadtest app

--- apps/loadtest/vite.config.ts
+++ apps/loadtest/vite.config.ts
@@ -3,5 +3,8 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  esbuild: {
+    target: "es2022", // Required for decorators
+  },
   plugins: [vanillaExtractPlugin(), solid({ ssr: true })],
 });
