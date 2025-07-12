978b0576d69d2ab4798cdb20c65cd0110f8891bd
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-12 10:33:13 +0200

    add missing context provider

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,12 @@ import "./assets/fonts";
 import { dark } from "@mp/style/themes/dark.css";
 import { lazy, Suspense } from "preact/compat";
 import { render } from "preact";
-import { ErrorBoundary, ErrorFallback, LoadingSpinner } from "@mp/ui";
+import {
+  ErrorBoundary,
+  ErrorFallback,
+  ErrorFallbackContext,
+  LoadingSpinner,
+} from "@mp/ui";
 import { assert } from "@mp/std";
 import * as styles from "./main.css";
 
@@ -18,12 +23,6 @@ rootElement.classList.add(styles.root);
 
 render(<Root />, rootElement);
 
-function handleError(error: unknown) {
-  // eslint-disable-next-line no-console
-  console.error(error);
-  return String(error);
-}
-
 function Root() {
   return (
     <>
@@ -32,11 +31,18 @@ function Root() {
         but it's here for safety so that if the error fallbacks in the inner error boundaries should fall, 
         this serves as a final bastion of hope to display our error message! 
         */}
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Suspense fallback={<LoadingSpinner debugId="main" />}>
-          <App />
-        </Suspense>
-      </ErrorBoundary>
+      <ErrorFallbackContext.Provider
+        value={{
+          // eslint-disable-next-line no-console
+          handleError: console.error,
+        }}
+      >
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<LoadingSpinner debugId="main" />}>
+            <App />
+          </Suspense>
+        </ErrorBoundary>
+      </ErrorFallbackContext.Provider>
     </>
   );
 }
