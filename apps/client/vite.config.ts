d675ad100909179ba67de49bd2bd2cdc1228a469
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-12 07:50:02 +0200

    enable decorators in preact

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -6,6 +6,11 @@ import preact from "@preact/preset-vite";
 
 import type { Plugin } from "vite";
 
+const babelPlugins = [
+  // esbuild already supports stage 3 decorators, but preact uses babel and not esbuild, so we need this plugin
+  ["@babel/plugin-proposal-decorators", { version: "2023-11" }],
+];
+
 export default defineConfig({
   esbuild: {
     target: "es2022", // Required for decorators
@@ -20,7 +25,7 @@ export default defineConfig({
     }),
     disallowExternalizingPlugin(),
     vanillaExtractPlugin(),
-    preact(),
+    preact({ babel: { plugins: babelPlugins } }),
     checker({ typescript: true }),
     ...(process.env.MP_CLIENT_EMBED_ENV ? [embedEnvPlugin()] : []),
   ],
--- apps/loadtest/vite.config.ts
+++ apps/loadtest/vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 
+const babelPlugins = [
+  // esbuild already supports stage 3 decorators, but preact uses babel and not esbuild, so we need this plugin
+  ["@babel/plugin-proposal-decorators", { version: "2023-11" }],
+];
+
 export default defineConfig({
   esbuild: {
     target: "es2022", // Required for decorators
   },
-  plugins: [vanillaExtractPlugin(), react()],
+  plugins: [
+    vanillaExtractPlugin(),
+    react({ babel: { plugins: babelPlugins } }),
+  ],
 });
