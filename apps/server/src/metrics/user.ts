a79ecd3cdefefb3764fa8973ad435cfbd61dd486
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-13 04:38:24 +0200

    enable user metrics again

--- apps/server/src/metrics/user.ts
+++ apps/server/src/metrics/user.ts
@@ -2,54 +2,56 @@ import type { MetricsRegistry } from "@mp/telemetry/prom";
 import { MetricsGague } from "@mp/telemetry/prom";
 import type { ClientRegistry, GameState } from "@mp/game/server";
 
-export function* collectUserMetrics(
+export function collectUserMetrics(
   registry: MetricsRegistry,
   clients: ClientRegistry,
   state: GameState,
 ) {
-  yield new MetricsGague({
-    name: "active_user_count",
-    help: "Number of users currently connected",
-    registers: [registry],
-    collect() {
-      this.set(clients.getUserCount());
-    },
-  });
+  return [
+    new MetricsGague({
+      name: "active_user_count",
+      help: "Number of users currently connected",
+      registers: [registry],
+      collect() {
+        this.set(clients.getUserCount());
+      },
+    }),
 
-  yield new MetricsGague({
-    name: "active_character_count",
-    help: "Number of player characters currently active",
-    registers: [registry],
-    collect() {
-      this.set(
-        state.actors
-          .values()
-          .filter((actor) => actor.type === "character")
-          .toArray().length,
-      );
-    },
-  });
+    new MetricsGague({
+      name: "active_character_count",
+      help: "Number of player characters currently active",
+      registers: [registry],
+      collect() {
+        this.set(
+          state.actors
+            .values()
+            .filter((actor) => actor.type === "character")
+            .toArray().length,
+        );
+      },
+    }),
 
-  yield new MetricsGague({
-    name: "active_npc_count",
-    help: "Number of non-player characters currently active",
-    registers: [registry],
-    collect() {
-      this.set(
-        state.actors
-          .values()
-          .filter((actor) => actor.type === "npc")
-          .toArray().length,
-      );
-    },
-  });
+    new MetricsGague({
+      name: "active_npc_count",
+      help: "Number of non-player characters currently active",
+      registers: [registry],
+      collect() {
+        this.set(
+          state.actors
+            .values()
+            .filter((actor) => actor.type === "npc")
+            .toArray().length,
+        );
+      },
+    }),
 
-  yield new MetricsGague({
-    name: "active_client_count",
-    help: "Number of active websocket connections",
-    registers: [registry],
-    collect() {
-      this.set(clients.getClientIds().size);
-    },
-  });
+    new MetricsGague({
+      name: "active_client_count",
+      help: "Number of active websocket connections",
+      registers: [registry],
+      collect() {
+        this.set(clients.getClientIds().size);
+      },
+    }),
+  ];
 }
