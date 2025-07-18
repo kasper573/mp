0e7a4d08edc4650a8686a4a47b0e8a70647af94d
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-18 10:56:05 +0200

    lower seeded npc count

--- apps/server/seed.ts
+++ apps/server/seed.ts
@@ -52,7 +52,7 @@ export async function seed(
           yield tx.insert(npcSpawnTable).values({
             npcType,
             areaId,
-            count: 25,
+            count: 6,
             id: createShortId() as NpcSpawnId,
             npcId: soldier.id,
           });
