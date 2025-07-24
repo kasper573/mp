5d3609ad5c7e32ee8fe8a09887e07d0b19c46310
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-24 13:00:03 +0200

    move use query signal to separat module

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,8 +1,3 @@
-import * as tanstack from "@tanstack/react-query";
-import { useSignal } from "@mp/state/react";
-import type { ReadonlySignal } from "@mp/state";
-import { useEffect, useMemo } from "preact/hooks";
-
 export {
   QueryClient,
   QueryClientProvider,
@@ -17,29 +12,5 @@ export {
   type UseQueryOptions,
   type DefaultError,
 } from "@tanstack/react-query";
-
-export function useQuerySignal<
-  TQueryFnData = unknown,
-  TError = tanstack.DefaultError,
-  TData = TQueryFnData,
-  TQueryKey extends tanstack.QueryKey = tanstack.QueryKey,
->(
-  options: tanstack.UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
-): Omit<tanstack.UseQueryResult<TData, TError>, "data"> & {
-  signal: ReadonlySignal<TData | undefined>;
-} {
-  const result = tanstack.useQuery(options);
-  const dataSignal = useSignal(result.data);
-  useEffect(() => {
-    dataSignal.value = result.data;
-  }, [result.data, dataSignal]);
-  return useMemo(
-    () => ({
-      ...result,
-      signal: dataSignal,
-    }),
-    [result, dataSignal],
-  );
-}
-
 export { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+export * from "./use-query-signal";
