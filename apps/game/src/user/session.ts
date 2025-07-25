d8a50e27a2dbd4e334252569e1213ddb1a425d6a
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-25 18:47:04 +0200

    remove unused id

--- apps/game/src/user/session.ts
+++ apps/game/src/user/session.ts
@@ -1,15 +1,11 @@
 import type { RoleDefinition, UserId } from "@mp/auth";
 import { InjectionContext } from "@mp/ioc";
 import type { CharacterId } from "../character/types";
-import type { Branded } from "@mp/std";
 
 export const ctxGameplaySession =
   InjectionContext.new<GameplaySession>("GameplaySession");
 
-export type GameplaySessionId = Branded<string, "GameplaySessionId">;
-
 export interface GameplaySession {
-  id: GameplaySessionId;
   userId: UserId;
   characterId: CharacterId;
   roles: ReadonlySetLike<RoleDefinition>;
