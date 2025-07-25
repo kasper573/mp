9d2396a04bc09dbe77ce08e4f95ebbd35cadd3e3
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-25 10:42:54 +0200

    dont expose all error details  (if so configured)

--- apps/api/src/integrations/trpc.ts
+++ apps/api/src/integrations/trpc.ts
@@ -1,8 +1,19 @@
 import type { ImmutableInjectionContainer } from "@mp/ioc";
 import { initTRPC } from "@trpc/server";
 import { transformer } from "../transformer";
+import { opt } from "../options";
 
-export const rpc = initTRPC.context<ApiContext>().create({ transformer });
+export const rpc = initTRPC.context<ApiContext>().create({
+  transformer,
+  errorFormatter(error) {
+    if (opt.exposeErrorDetails) {
+      return error;
+    }
+    // Omit the sensitive error details
+    const { shape: _, ...alwaysSafeToExpose } = error;
+    return alwaysSafeToExpose;
+  },
+});
 
 export interface ApiContext {
   ioc: ImmutableInjectionContainer;
