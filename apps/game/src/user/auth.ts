662eae9ee9defe4135a2a1207eab490e47eeb5cb
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-25 12:00:17 +0200

    remove unsed functions

--- apps/game/src/user/auth.ts
+++ apps/game/src/user/auth.ts
@@ -1,25 +1,19 @@
-import type { RoleDefinition, UserId } from "@mp/auth";
+import type { RoleDefinition } from "@mp/auth";
 import { evt } from "../network/event-builder";
 import { ctxClientRegistry } from "./client-registry";
 import { ctxClientId } from "./client-id";
 import { ctxGameStateLoader } from "../game-state/game-state-loader";
 
-export function auth() {
-  return evt.middleware(({ ctx }): AuthContext => {
+export function roles(requiredRoles: RoleDefinition[]) {
+  return evt.middleware(async ({ ctx, mwc }) => {
     const clientId = ctx.get(ctxClientId);
     const clients = ctx.get(ctxClientRegistry);
     const userId = clients.userIds.get(clientId);
     if (userId === undefined) {
       throw new Error("User not authenticated");
     }
-    return { userId };
-  });
-}
-
-export function roles(requiredRoles: RoleDefinition[]) {
-  return auth().pipe(async ({ ctx, mwc }) => {
     const loader = ctx.get(ctxGameStateLoader);
-    const roles = await loader.getUserRoles(mwc.userId);
+    const roles = await loader.getUserRoles(userId);
     const requiredRolesSet = new Set(requiredRoles);
     if (!requiredRolesSet.isSubsetOf(roles)) {
       const missingRoles = requiredRolesSet.difference(roles);
@@ -27,20 +21,6 @@ export function roles(requiredRoles: RoleDefinition[]) {
         "Missing permissions: " + missingRoles.values().toArray().join(", "),
       );
     }
-
     return mwc;
   });
-}
-
-export function optionalAuth() {
-  return evt.middleware(({ ctx }): Partial<AuthContext> => {
-    const clientId = ctx.get(ctxClientId);
-    const clients = ctx.get(ctxClientRegistry);
-    const userId = clients.userIds.get(clientId);
-    return { userId };
-  });
-}
-
-export interface AuthContext {
-  userId: UserId;
 }
