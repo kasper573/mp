146b20198d6d404e9e84313384ff9cea49b7bb30
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-27 20:55:39 +0200

    remove excessive warnings

--- libraries/math/src/polygon.ts
+++ libraries/math/src/polygon.ts
@@ -4,7 +4,5 @@ export function polygonHitTest<T extends number>(
   _1: unknown,
   _2: VectorLike<T>,
 ): boolean {
-  //  oxlint-disable-next-line no-console
-  console.warn("polygonHitTest not implemented");
   return false;
 }
--- libraries/math/src/polyline.ts
+++ libraries/math/src/polyline.ts
@@ -4,7 +4,5 @@ export function polylineHitTest<T extends number>(
   _1: unknown,
   _2: VectorLike<T>,
 ): boolean {
-  // oxlint-disable-next-line no-console
-  console.warn("polylineHitTest not implemented");
   return false;
 }
