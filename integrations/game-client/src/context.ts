38b682061f0ecd5c0b9af460eb08e0308f7e4863
Kasper <805006+kasper573@users.noreply.github.com>
2025-07-31 03:54:40 +0200

    more formatting

--- integrations/game-client/src/context.ts
+++ integrations/game-client/src/context.ts
@@ -6,15 +6,19 @@ import type { TiledSpritesheetRecord } from "@mp/tiled-renderer";
 import type { GameStateClient } from "./game-state-client";
 
 export const ctxEngine = InjectionContext.new<Engine>("Engine");
+
 export const ctxGameEventClient =
   InjectionContext.new<GameEventClient>("GameEventClient");
+
 export const ctxGameStateClient =
   InjectionContext.new<GameStateClient>("GameStateClient");
+
 export const ctxAreaSpritesheets = InjectionContext.new<TiledSpritesheetRecord>(
   "TiledSpritesheetRecord",
 );
 export const ctxActorSpritesheetLookup =
   InjectionContext.new<ActorSpritesheetLookup>("ActorSpritesheetLookup");
+
 export const ctxActorModelLookup =
   InjectionContext.new<ActorModelLookup>("ActorModelLookup");
 
