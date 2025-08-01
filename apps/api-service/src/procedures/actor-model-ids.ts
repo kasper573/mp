c8fe2db56491add80766ac56d7c7d596f40db0aa
Kasper <805006+kasper573@users.noreply.github.com>
2025-08-01 20:40:04 +0200

    api service should resolve actor model ids from database

--- apps/api-service/src/procedures/actor-model-ids.ts
+++ apps/api-service/src/procedures/actor-model-ids.ts
@@ -1,10 +1,14 @@
-import type { ActorModelId } from "@mp/db/types";
+import { actorModelTable } from "@mp/db";
 import { type } from "@mp/validate";
+import { ctxDbClient } from "../context";
 import { rpc } from "../integrations/trpc";
 
 export const actorModelIds = rpc.procedure
   .output(type("string").brand("ActorModelId").array())
-  .query(() => {
-    // TODO should be in database
-    return ["adventurer"] as ActorModelId[];
+  .query(async ({ ctx }) => {
+    const db = ctx.ioc.get(ctxDbClient);
+    const rows = await db
+      .select({ id: actorModelTable.id })
+      .from(actorModelTable);
+    return rows.map((row) => row.id);
   });
