84f9da1f3e6fc0e125bf514d80f18f7bd50df36b
Kasper <805006+kasper573@users.noreply.github.com>
2025-08-05 00:29:10 +0200

    signal map doesn't need to extend from Map

--- libraries/state/src/signal-map.ts
+++ libraries/state/src/signal-map.ts
@@ -1,61 +1,53 @@
 import { NotifiableSignal } from "./custom-signals/notifiable-signal";
 
-export class SignalMap<K, V> extends Map<K, V> {
+export class SignalMap<K, V> {
   #signal: NotifiableSignal<Map<K, V>>;
 
   constructor(entries?: Iterable<readonly [K, V]> | null) {
-    // Omit entries from super call since we store values in a signal
-    super();
     this.#signal = new NotifiableSignal(new Map<K, V>(entries));
   }
 
-  override clear(): void {
+  clear(): void {
     const map = this.#signal.value;
     if (map.size > 0) {
       map.clear();
       this.#signal.notify();
     }
   }
-  override delete(key: K): boolean {
+  delete(key: K): boolean {
     const deleted = this.#signal.value.delete(key);
     if (deleted) {
       this.#signal.notify();
     }
     return deleted;
   }
-  override forEach<ThisArg>(
-    callbackfn: (value: V, key: K, map: Map<K, V>) => void,
-    thisArg?: ThisArg,
-  ): void {
-    this.#signal.value.forEach(callbackfn, thisArg);
-  }
-  override get(key: K): V | undefined {
+  get(key: K): V | undefined {
     return this.#signal.value.get(key);
   }
-  override has(key: K): boolean {
+  has(key: K): boolean {
     return this.#signal.value.has(key);
   }
-  override set(key: K, value: V): this {
+  set(key: K, value: V): this {
     this.#signal.value.set(key, value);
     this.#signal.notify();
     return this;
   }
-  override get size(): number {
+  get size(): number {
     return this.#signal.value.size;
   }
-  override entries(): MapIterator<[K, V]> {
+  entries(): MapIterator<[K, V]> {
     return this.#signal.value.entries();
   }
-  override keys(): MapIterator<K> {
+  keys(): MapIterator<K> {
     return this.#signal.value.keys();
   }
-  override values(): MapIterator<V> {
+  values(): MapIterator<V> {
     return this.#signal.value.values();
   }
-  override [Symbol.iterator](): MapIterator<[K, V]> {
+  [Symbol.iterator](): MapIterator<[K, V]> {
     return this.#signal.value[Symbol.iterator]();
   }
-  override get [Symbol.toStringTag](): string {
+  get [Symbol.toStringTag](): string {
     return this.#signal.value[Symbol.toStringTag];
   }
 }
