9fbbaa69f065f07a1c4ec448a95dc3b74841675b
Kasper <805006+kasper573@users.noreply.github.com>
2025-08-06 07:20:31 +0200

    add plugin to loadtest vite config as well

--- apps/loadtest/vite.config.ts
+++ apps/loadtest/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vite";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { preact } from "@preact/preset-vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { defineConfig } from "vite";
+
+const babelPlugins = [
+  // esbuild already supports stage 3 decorators, but preact uses babel and not esbuild, so we need this plugin
+  ["@babel/plugin-proposal-decorators", { version: "2023-11" }],
+];
 
 export default defineConfig({
   esbuild: {
     target: "es2022", // Required for decorators
   },
-  plugins: [vanillaExtractPlugin(), preact()],
+  plugins: [
+    vanillaExtractPlugin(),
+    preact({ babel: { plugins: babelPlugins } }),
+  ],
 });
