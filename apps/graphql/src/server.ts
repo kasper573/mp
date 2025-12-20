026fc8fca65ca42752b800e8c5197f868cf8e125
Kasper <805006+kasper573@users.noreply.github.com>
2025-12-20 21:35:52 +0100

    set up POC apollo server

---
+++ apps/graphql/src/server.ts
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import createExpressApp, { json } from "express";
+import http from "http";
+import cors from "cors";
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const express = createExpressApp();
+const httpServer = http.createServer(express);
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await apolloServer.start();
+
+express.use(cors(), json(), expressMiddleware(apolloServer));
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000`);
