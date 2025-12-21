bbde0008e291500c38bf8ad2eefee93f19a2da16
Kasper <805006+kasper573@users.noreply.github.com>
2025-12-21 07:12:37 +0100

    fix lint issues

--- apps/api-service/server/integrations/apollo-request-logger.ts
+++ apps/api-service/server/integrations/apollo-request-logger.ts
@@ -1,17 +1,18 @@
+// oxlint-disable require-await
 import type { ApolloServerPlugin } from "@apollo/server";
 import type { ApiContext } from "../context";
 import { ctxLogger } from "../context";
 
 export function apolloRequestLoggerPlugin(): ApolloServerPlugin<ApiContext> {
   return {
-    requestDidStart({
+    async requestDidStart({
       contextValue: { ioc },
       request: { http: _, ...request },
     }) {
       const logger = ioc.get(ctxLogger);
       logger.info(request, "GraphQL request received");
       return {
-        didEncounterErrors({ errors }) {
+        async didEncounterErrors({ errors }) {
           for (const error of errors) {
             logger.error(
               new Error(
