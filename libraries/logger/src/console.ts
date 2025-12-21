067f8dadf23a50225e6c6ea08b906b69d04e0cf8
Kasper <805006+kasper573@users.noreply.github.com>
2025-12-21 18:55:12 +0100

    make console logger a bit prettier

--- libraries/logger/src/console.ts
+++ libraries/logger/src/console.ts
@@ -4,11 +4,13 @@ import type { Logger } from "./abstract";
 export function createConsoleLogger(
   bindings?: Record<string, unknown>,
 ): Logger {
+  // Use an args spread to avoid an ungly "undefined" being printed in some environments.
+  const bindingArgs = bindings ? [bindings] : [];
   return {
-    info: (...args: unknown[]) => console.info(...args, bindings),
-    warn: (...args: unknown[]) => console.warn(...args, bindings),
-    error: (...args: unknown[]) => console.error(...args, bindings),
-    debug: (...args: unknown[]) => console.debug(...args, bindings),
+    info: (...args: unknown[]) => console.info(...args, ...bindingArgs),
+    warn: (...args: unknown[]) => console.warn(...args, ...bindingArgs),
+    error: (...args: unknown[]) => console.error(...args, ...bindingArgs),
+    debug: (...args: unknown[]) => console.debug(...args, ...bindingArgs),
     child: (childBindings) =>
       createConsoleLogger({ ...bindings, ...childBindings }),
   };
