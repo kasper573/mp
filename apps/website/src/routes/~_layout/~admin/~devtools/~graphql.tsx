8af8d9c8e2a7f976892e1a324308d1d6710ffb1e
Kasper <805006+kasper573@users.noreply.github.com>
2025-12-21 07:03:28 +0100

    remove graphql tester route

--- apps/website/src/routes/~_layout/~admin/~devtools/~graphql.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { GraphQLTester } from "./GraphQLTester";
-
-export const Route = createFileRoute("/_layout/admin/devtools/graphql")({
-  component: GraphQLTester,
-});
