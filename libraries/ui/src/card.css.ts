7d134017b670441f1c0d7458fe916944389d95cc
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-06 18:33:02 +0100

    add card variants

--- libraries/ui/src/card.css.ts
+++ libraries/ui/src/card.css.ts
@@ -2,14 +2,29 @@ import { atoms, recipe } from "@mp/style";
 
 export const card = recipe({
   base: atoms({
-    backgroundColor: "info.base",
-    color: "info.face",
     borderRadius: "m",
     padding: "l",
   }),
   variants: {
+    intent: {
+      info: atoms({
+        backgroundColor: "info.base",
+        color: "info.face",
+      }),
+      error: atoms({
+        backgroundColor: "error.base",
+        color: "error.face",
+      }),
+      success: atoms({
+        backgroundColor: "success.base",
+        color: "success.face",
+      }),
+    },
     floating: {
       true: atoms({ boxShadow: "m" }),
     },
+  },
+  defaultVariants: {
+    intent: "info",
   },
 });
