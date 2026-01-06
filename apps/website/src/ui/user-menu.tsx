d788a9218a910bddea671c9910159176e4db6b59
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-06 16:59:43 +0100

    update user menu with new popover and card components

--- apps/website/src/ui/user-menu.tsx
+++ apps/website/src/ui/user-menu.tsx
@@ -1,6 +1,15 @@
 import { useContext } from "preact/hooks";
 import { AuthContext } from "../integrations/contexts";
-import { Button } from "@mp/ui";
+import {
+  Button,
+  Card,
+  Link,
+  PopoverClose,
+  PopoverContent,
+  PopoverPortal,
+  PopoverRoot,
+  PopoverTrigger,
+} from "@mp/ui";
 
 export function UserMenu() {
   const auth = useContext(AuthContext);
@@ -13,25 +22,26 @@ export function UserMenu() {
     );
   }
 
-  function onSelect(e: preact.TargetedEvent<HTMLSelectElement>) {
-    switch (e.currentTarget?.value) {
-      case "profile":
-        window.location.href = auth.getAccountConsoleUrl();
-        break;
-      case "signout":
-        void auth.signOutRedirect();
-        break;
-    }
-  }
-
-  // This is a simple user menu that assumes (and only works if) the user is redirected after making a selection.
   return (
-    <select onChange={onSelect} value="name">
-      <option value="name" disabled>
-        {auth.identity.value?.name}
-      </option>
-      <option value="profile">User profile</option>
-      <option value="signout">Sign out</option>
-    </select>
+    <PopoverRoot>
+      <PopoverTrigger asChild>
+        <Button>{auth.identity.value?.name}</Button>
+      </PopoverTrigger>
+      <PopoverPortal>
+        <PopoverContent>
+          <Card floating>
+            <PopoverClose asChild>
+              <Link href={auth.getAccountConsoleUrl()}>Account</Link>
+            </PopoverClose>
+            <br />
+            <PopoverClose asChild>
+              <Link href="#" onClick={() => void auth.signOutRedirect()}>
+                Sign out
+              </Link>
+            </PopoverClose>
+          </Card>
+        </PopoverContent>
+      </PopoverPortal>
+    </PopoverRoot>
   );
 }
