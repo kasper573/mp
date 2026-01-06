04c64c249a92c9b67b17f6394b8fb283c37ecff7
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-06 18:33:11 +0100

    add disabled variant to button

--- libraries/ui/src/button.css.ts
+++ libraries/ui/src/button.css.ts
@@ -1,17 +1,28 @@
-import { atoms } from "@mp/style";
+import { atoms, recipe } from "@mp/style";
 
-export const button = atoms({
-  all: "unset",
-  backgroundColor: {
-    default: "primary.base",
-    hover: "primary.hover",
-    active: "primary.base",
+export const button = recipe({
+  base: atoms({
+    all: "unset",
+    backgroundColor: {
+      default: "primary.base",
+      hover: "primary.hover",
+      active: "primary.base",
+    },
+    color: "primary.face",
+    border: "thin",
+    borderColor: "primary.face_subtle",
+    px: "l",
+    py: "m",
+    borderRadius: "m",
+    cursor: "pointer",
+  }),
+  variants: {
+    disabled: {
+      true: atoms({
+        opacity: 0.4,
+        pointerEvents: "none",
+        userSelect: "none",
+      }),
+    },
   },
-  color: "primary.face",
-  border: "thin",
-  borderColor: "primary.face_subtle",
-  px: "l",
-  py: "m",
-  borderRadius: "m",
-  cursor: "pointer",
 });
