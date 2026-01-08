3b54f2ba6d7b18ef89344de651622037f46b63c5
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-08 20:43:38 +0100

    store portals in area resource

--- integrations/game-shared/src/area.ts
+++ integrations/game-shared/src/area.ts
@@ -22,6 +22,8 @@ export class AreaResource {
    */
   readonly dynamicLayer: Layer;
 
+  readonly portals: ReadonlyArray<Portal>;
+
   constructor(
     readonly id: AreaId,
     readonly tiled: TiledResource,
@@ -38,9 +40,13 @@ export class AreaResource {
       "Invalid area data: must have a start location",
     );
 
+    const portals: Portal[] = [];
     for (const obj of tiled.objects) {
       const res = getDestinationFromObject(obj);
-      if (res?.isErr()) {
+      if (!res) {
+        continue;
+      }
+      if (res.isErr()) {
         throw new Error(
           `Area "${id}" tiled map has an invalid portal object "${obj.id}"`,
           {
@@ -48,7 +54,11 @@ export class AreaResource {
           },
         );
       }
+
+      portals.push({ destination: res.value, object: obj });
     }
+
+    this.portals = portals;
 
     this.start = tiled.worldCoordToTile(Vector.from(startObj)).round();
   }
@@ -91,9 +101,14 @@ export function getDestinationFromObject(
   });
 }
 
+interface Portal {
+  readonly destination: PortalDestination;
+  readonly object: TiledObject;
+}
+
 export interface PortalDestination {
-  areaId: AreaId;
-  coords: Vector<Tile>;
+  readonly areaId: AreaId;
+  readonly coords: Vector<Tile>;
 }
 
 export const dynamicLayerName = "Dynamic";
