92f706f837f07be91bc58b1127f43d2d63cd029d
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-24 23:50:45 +0100

    add auth to countdown subscription

--- apps/api-service/server/modules/countdown.ts
+++ apps/api-service/server/modules/countdown.ts
@@ -1,8 +1,15 @@
 import type { Int } from "grats";
+import { auth } from "../integrations/auth";
+import type { ApiContext } from "../context";
+import { ctxLogger } from "../context";
 
 /** @gqlSubscriptionField */
-export async function* countdown(): AsyncIterable<Int> {
+export async function* countdown(ctx: ApiContext): AsyncIterable<Int> {
+  const logger = ctx.ioc.get(ctxLogger);
+  const res = await auth(ctx);
+  logger.info(`${res.user.name} started countdown`);
   for (let i = 10; i >= 0; i--) {
+    logger.info(`${res.user.name} countdown at ${i}`);
     // oxlint-disable-next-line no-await-in-loop
     await sleep(1);
     yield i;
