e9629ad8ae0b44cb7b578c07efe00a68d9f80c36
Kasper <805006+kasper573@users.noreply.github.com>
2026-01-25 02:32:37 +0100

    expose new state utils

--- libraries/state/src/index.ts
+++ libraries/state/src/index.ts
@@ -3,4 +3,6 @@ export * from "./custom-signals/property-signal";
 export * from "./custom-signals/storage-signal";
 export * from "./memory-storage";
 export * from "./signal";
+export * from "./signal-async-iterable";
+export * from "./compute-set-changes";
 export * from "./universal-storage";
