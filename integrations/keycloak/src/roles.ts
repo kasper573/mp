4dc9dd759af69f2809d02c6a3392c93eba6d0e24
Kasper <805006+kasper573@users.noreply.github.com>
2026-05-10 09:40:31 +0200

    rename roles

--- integrations/keycloak/src/roles.ts
+++ integrations/keycloak/src/roles.ts
@@ -8,11 +8,7 @@ export const systemRoles = defineRoles("sys", [
   "useDevTools",
 ]);
 
-export const gameRoles = defineRoles("gateway", [
-  "spectate",
-  "join",
-  "gameServiceBroadcast",
-]);
+export const userRoles = defineRoles("user", ["join", "spectate"]);
 
 export const characterRoles = defineRoles("character", [
   "move",
@@ -31,13 +27,13 @@ export const playerRoles = [
   characterRoles.attack,
   characterRoles.respawn,
   characterRoles.recall,
-  gameRoles.join,
+  userRoles.join,
 ];
 
 export const groupedRoles = {
   admin: [
     ...Object.values(systemRoles),
-    ...Object.values(gameRoles),
+    ...Object.values(userRoles),
     ...Object.values(characterRoles),
     ...Object.values(npcRoles),
   ],
